c6f44ef27544d0255b3f4b7c76047d742709c6d8
Jair Salcedo <salcedojair3@gmail.com>
2021-03-26 17:51:57 -0600

    version 1

---
+++ main.js
@@ -0,0 +1,53 @@
+
+var nombre = "Victor Robles";
+var altura = 190;
+
+/*
+var concatenacion = nombre + " " + altura;
+
+var datos = document.getElementById("datos");
+//datos.innerHTML = concatenacion;
+datos.innerHTML = `
+    <h1>Soy la caja de datos</h1>
+    <h2>Mi nombre es: ${nombre}</h2>
+    <h3>Mido: ${altura}</h3>
+`;
+
+if(altura => 190){
+    datos.innerHTML += '<h1>Eres una persona ALTA</h1>';
+}else{
+    datos.innerHTML += '<h1>Eres una persona BAJITA</h1>'; 
+}
+
+
+for(var i = 2220; i<=2020; i++){
+    //Bloque de instrucciones
+    datos.innerHTML += "<h2>Estamos en el año: " + i;
+}
+*/
+
+function MuestraMiNombre(nombre, altura){
+    var datos = document.getElementById("datos");
+    //datos.innerHTML = concatenacion;
+    datos.innerHTML = `
+        <h1>Soy la caja de datos</h1>
+        <h2>Mi nombre es: ${nombre}</h2>
+        <h3>Mido: ${altura}</h3>
+    `;
+
+    return misDatos;
+}
+
+function imprimir(){
+    var datos = document.getElementById("datos");
+    datos.innerHTML = MuestraMiNombre("Victor Robles WEB", 190);
+}
+
+imprimir();
+
+var nombres = ["Victor", "Antonio", "Joaquin",];
+
+document.write('<h1>Listado de nombres</h1>');
+for(i = 0; i < nombres.length; i++){
+    document.write(nombres[i] + '<br/>');
+}
